0d43655e2504df0ab1d322d04be71cc0efa40965
Dustin Branson <Dustin.Branson@vu.com>
2024-02-06 16:54:57 -0600

    maybe

--- payCalculator.py
+++ payCalculator.py
@@ -2,11 +2,11 @@ def calculatePay():
     # Implement your solution in between the two comment blocks
     print("calculating pay")
     # This first line is provided for you
-hrs = float(input("Enter Hours:"))
-rate = float(input("Enter Rate:"))
-pay = hrs*rate
+hrs = int(input("enter hours:"))
+rate = float(input("enter rate:"))
+pay = (hrs*rate)
 
-print("Pay:",pay)
+print(pay)
 
 
 
